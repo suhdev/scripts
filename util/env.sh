55dfbb6c9393fb81468fd69996b599b8f0bc91d8
Suhail Abood <suhail.abood@gmail.com>
2023-05-13 12:36:49 +0100

    Update env functions

--- util/env.sh
+++ util/env.sh
@@ -15,3 +15,26 @@ add-to-global-path() {
     fi
   fi
 }
+
+add-source-file() {
+  local p="$1"
+
+  # check if the path is not empty 
+  if [ -z "$p" ]; then
+    echo "please specify a source file to source in your zshrc"
+    exit 1
+  fi
+
+  p=$(realpath $p)
+
+  if [ -f "$p" ]; then # check if the folder exists
+    if ! grep -iq "source ${p}" $HOME/.zshrc; then # check if the path is already in the PATH
+      echo "" >> $HOME/.zshrc
+      echo "# Adding source file" >> $HOME/.zshrc
+      echo "if [ -f \"${p}\" ]; then" >> $HOME/.zshrc
+      echo "  source ${p}" >> $HOME/.zshrc
+      echo "fi" >> $HOME/.zshrc
+      source ${p}
+    fi
+  fi
+}
